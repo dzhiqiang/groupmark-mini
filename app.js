dd015b521108499db20ad6c92fe65e086bfe5df0
dzhiqiang <584462393@qq.com>
2019-10-20 08:45:05 +0800

    增加登录

--- app.js
+++ app.js
@@ -1,39 +1,45 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    this.onLogin();
+  },
+  globalData: {
+    userInfo: null
+  },
+  onLogin:function(){
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        if (res) {
           wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
+            success: userInfoRes => {
+              var userInfo = userInfoRes.userInfo;
+              this.globalData.userInfo = userInfoRes.userInfo;
               if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+                this.userInfoReadyCallback(userInfoRes)
               }
+              wx.request({
+                url: 'http://localhost:8080/groupmark/login/login',
+                method: 'POST',
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                },
+                data: {
+                  code: res.code,
+                  nickName: userInfo.nickName
+                },
+                dataType: 'json',
+                success(loginRes) {
+                  if (loginRes.data.code == '0000'){
+                    wx.setStorageSync('token', loginRes.data.token)
+                  }
+                }
+              })
             }
-          })
+          });
         }
       }
     })
-  },
-  globalData: {
-    userInfo: null
   }
 })
