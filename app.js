d00f21799f65d7df88defa60ee27768705baf9ea
dzhiqiang <584462393@qq.com>
2019-10-20 21:37:56 +0800

    增加登录

--- app.js
+++ app.js
@@ -12,44 +12,36 @@ App({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         if (res) {
-          wx.getSetting({
-            success: res => {
-              if (res.authSetting['scope.userInfo']) {
-                // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                wx.getUserInfo({
-                  success: res => {
-                    // 可以将 res 发送给后台解码出 unionId
-                    this.globalData.userInfo = res.userInfo
-                    // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-                    // 所以此处加入 callback 以防止这种情况
-                    if (this.userInfoReadyCallback) {
-                      this.userInfoReadyCallback(res)
-                    }
-
+          wx.getUserInfo({
+            success: userInfoRes => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = userInfoRes.userInfo;
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(userInfoRes);
+              }
+              wx.request({
+                url: 'http://localhost:8080/groupmark/login/login',
+                method: 'POST',
+                header: {
+                  'content-type': 'application/x-www-form-urlencoded'
+                },
+                data: {
+                  code: res.code,
+                  nickName:userInfoRes.userInfo.nickName
+                },
+                dataType: 'json',
+                success(loginRes) {
+                  if (loginRes.data.code == '0000') {
+                    wx.setStorageSync('token', loginRes.data.token)
                   }
-                })
-              }
-            }
-          })
-          wx.request({
-            url: 'http://localhost:8080/groupmark/login/login',
-            method: 'POST',
-            header: {
-              'content-type': 'application/x-www-form-urlencoded'
-            },
-            data: {
-              code: res.code
-            },
-            dataType: 'json',
-            success(loginRes) {
-              if (loginRes.data.code == '0000') {
-                wx.setStorageSync('token', loginRes.data.token)
-              }
+                }
+              })
             }
           })
         }
       }
     })
-    
   }
 })
